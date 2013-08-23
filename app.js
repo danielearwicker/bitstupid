f125a81e533b5c6dba764d3f1ad8a3638aa83e74
Daniel Earwicker <dan@earwicker.com>
2013-08-23 23:18:00 +0100

    Simple image uploading to Amazon S3

--- app.js
+++ app.js
@@ -1,24 +1,129 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var AWS = require('aws-sdk');
 var views = require('./views');
+
+var log = console.log;
+// var log = function() {}
+
+// Load AWS configuration
+AWS.config.loadFromPath(path.join(__dirname, '../../aws-config.json'));
+log(AWS.config.credentials);
+
+// Object.keys doesn't get inherited keys
+var getAllKeys = function(obj) {
+    var keys = [];
+    for (var key in obj) {
+        keys.push(key);
+    }
+    return keys;
+};
+
+// Wrap S3 API in verbose logging
+var actualS3 = new AWS.S3({ apiVersion: '2006-03-01' });
+var s3 = {};
+getAllKeys(actualS3).forEach(function(key) {
+    s3[key] = function() {
+        log('Calling Amazon S3: ' + key);
+        log(arguments);
+        actualS3[key].apply(actualS3, arguments);
+    };
+});
 
 var app = express();
 
-app.use("/img", express.static('img'));
+// app.use("/static", express.static('static'));
+
+app.use(express.bodyParser());
+
+// Utility that standardising error handling
+var ifGood = function(res, handler) {
+    return function(err, data) {
+        if (err) {
+            log(err.message);
+            res.send(500, err.message);
+        } else {
+            handler(data);
+        }
+    };
+};
+
+var username = 'danielearwicker';
 
 app.get('/', function(req, res) {
     res.send(views.home({ age: 41 }));
 });
 
-app.get('/togglz/create', function(req, res) {
-    res.send(views.home({ age: 41 }));
+app.get('/create', function(req, res) {
+    var images = {};
+    [0, 1].forEach(function(image) {
+        var key = 'creating-' + username + '-' + image;
+        s3.headObject({
+            Bucket: 'bitstupid-images',
+            Key: key
+        }, function(err) {
+            images[image] = err ? null : key;
+            if (Object.keys(images).length == 2) {
+                res.send(views.create({ images: images }));
+            }
+        });
+    });
 });
 
-/*
+app.get('/images/:imgkey', function(req, res) {
+    s3.getObject({
+        Bucket: 'bitstupid-images',
+        Key: req.params.imgkey
+    },
+    ifGood(res, function(data) {
+        res.set('Content-Type', data.ContentType);
+        res.send(data.Body);
+    }));
+});
+
+app.post('/images', function(req, res) {
+    var finishedCount = 0;
+
+    var finished = function() {
+        finishedCount++;
+        if (finishedCount == 2) {
+            log('Both stores completed successfully');
+            res.redirect('create');
+        }
+    };
+
+    [0, 1].forEach(function(image) {
+        fs.readFile(req.files['image' + image].path, ifGood(res, function(body) {
+
+            var contentType = req.files['image' + image].type;
+            if (contentType &&
+                contentType.match(/^image\//) &&
+                (body.length != 0)) {
+
+                s3.putObject({
+                        Bucket: 'bitstupid-images',
+                        Body: body,
+                        ContentType: contentType,
+                        Key: 'creating-' + username + '-' + image
+                    },
+                    ifGood(res, finished)
+                );
+            } else {
+                finished();
+            }
+        }));
+    });
+});
+
+
 app.get('/:username/:id/state', function(req, res) {
 
-    var key = req.query.username + ':' + req.query.id;
+    var key = req.params.username + ':' + req.params.id;
 
-    redis.llen(key, function(err, count) {
+    log(req);
+
+/*    redis.llen(key, function(err, count) {
         if (err) {
             res.send(500, err.message);
         } else {
@@ -47,11 +152,13 @@ app.get('/:username/:id/state', function(req, res) {
             }
         }
     });
+    */
 });
 
+/*
 app.post('/:username/:id/state', function(req, res) {
     redis.lpush(
-        req.query.username + ':' + req.query.id,
+        req.params.username + ':' + req.params.id,
         new Date().toISOString(),
         function(err, count) {
             if (err) {
